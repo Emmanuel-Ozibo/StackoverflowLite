2a45a76ae36943bb03092ed9ed2ab7a8b15eedf1
Emeecodes001 <emmanuel022forreal@gmail.com>
2018-09-07 04:28:10 +0100

    dynamic database access to fit heroku database

--- database.js
+++ database.js
@@ -1,8 +1,13 @@
 const{Pool, Client} = require('pg')
+const config = require('config')
+
+
 const pool = new Pool({
-    user: 'postgres', 
-    database: 'sofDb', 
-    password: `${process.env.PASSWORD}`, 
-    port: 5432
+    user: `${config.get('db_USER')}`, 
+    host: `${config.get('db_HOST')}`,
+    database: `${config.get('db_NAME')}`, 
+    password: `${config.get('db_PASSWORD')}`, 
+    port: config.get('db_PORT')
 })
+console.log(`${config.get('db_USER')}`)
 module.exports = pool
